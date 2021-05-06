a11be2545384518adeafb481149fe79a3cd63954
Emanuel Tesar <e.tesarr@gmail.com>
2021-05-06 15:42:29 +0200

    Pin specific DAO contracts repo

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -36,7 +36,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.2',
+        version: '0.8.4',
         settings: {
           optimizer: {
             enabled: true,
