2f545a20f47226bfda2df0a50e0e6012ab650ee8
Emanuel Tesar <e.tesarr@gmail.com>
2021-05-06 14:36:33 +0200

    Fix yarn.lock and App.tsx after bad rebase

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,6 @@ const Proposals = () => {
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
     <Router>
       {/* TODO: remove */}
       <nav>
@@ -54,7 +41,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
->>>>>>> Add router
   );
 }
 
