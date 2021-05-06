6c292c7984d6fd3ee3eb1467e55ee77da9b011c5
Emanuel Tesar <e.tesarr@gmail.com>
2021-05-06 13:50:44 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+const Dashboard = () => {
+  return <div>dashboard</div>;
+};
+
+const Proposals = () => {
+  return <div>proposals</div>;
+};
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -15,6 +24,37 @@ function App() {
         </a>
       </header>
     </div>
+=======
+    <Router>
+      {/* TODO: remove */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/proposals">Proposals</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* NOTE: The first matching root is used. This imples that `/` must be last */}
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/proposals">
+          <Proposals />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+>>>>>>> Add router
   );
 }
 
