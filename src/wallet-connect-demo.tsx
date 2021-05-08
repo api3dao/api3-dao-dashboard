31b807d56064ce57d517eacdb530bd7aa90e9204
Emanuel Tesar <e.tesarr@gmail.com>
2021-05-08 11:28:21 +0200

    Proper wallet connect connection/disconectio handling

--- src/wallet-connect-demo.tsx
+++ src/wallet-connect-demo.tsx
@@ -11,6 +11,17 @@ const daoNetworks = [localhostDao, ropstenDao];
 const WalletConnectDemo = () => {
   const { setChainData, provider, ...data } = useChainData();
 
+  const onDisconnect = () => {
+    if (provider) {
+      const externalProvider: any = provider.provider;
+      if (typeof externalProvider.close === 'function') {
+        externalProvider.close();
+      }
+    }
+
+    setChainData(initialChainData);
+  };
+
   const onWalletConnect = async () => {
     const web3Modal = new Web3Modal({
       // If true, it the provider will be cached in local storage and there will be no modal
@@ -33,18 +44,9 @@ const WalletConnectDemo = () => {
       },
     });
 
-    const wcProvider = await web3Modal.connect();
-
-    try {
-      // Enable session (triggers QR Code modal)
-      await wcProvider.enable();
-    } catch {
-      // TODO: handle error (e.g. user closes the modal)
-      return;
-    }
-
+    const web3ModalProvider = await web3Modal.connect();
     const upsertData = async () => {
-      const provider = new ethers.providers.Web3Provider(wcProvider);
+      const provider = new ethers.providers.Web3Provider(web3ModalProvider);
       const networkChainId = await (await provider.getNetwork()).chainId.toString();
 
       const daoNetwork = daoNetworks.find(({ chainId }) => chainId === networkChainId);
@@ -60,23 +62,32 @@ const WalletConnectDemo = () => {
       setChainData({ ...newData, provider });
     };
 
-    wcProvider.on('accountsChanged', () => {
+    try {
+      // Enable session (triggers QR Code modal)
+      await web3ModalProvider.enable();
+      // User has chosen a provider and has signed in
+      upsertData();
+    } catch {
+      // TODO: handle error (e.g. user closes the modal)
+      return;
+    }
+
+    web3ModalProvider.on('accountsChanged', () => {
       upsertData();
     });
 
-    wcProvider.on('chainChanged', () => {
+    web3ModalProvider.on('chainChanged', () => {
       upsertData();
     });
 
-    wcProvider.on('networkChanged', () => {
+    web3ModalProvider.on('networkChanged', () => {
       upsertData();
     });
 
-    upsertData();
-  };
-
-  const onDisconnect = () => {
-    setChainData(initialChainData);
+    // NOTE: This callback might get called multiple times for a single disconnect
+    web3ModalProvider.on('disconnect', () => {
+      onDisconnect();
+    });
   };
 
   let contractsData = 'Unsupported chain!';
