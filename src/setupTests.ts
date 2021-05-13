798f94dfebbaeb99387b4347eaeae79d2535b44c
Emanuel Tesar <e.tesarr@gmail.com>
2021-05-13 10:38:08 +0200

    Change favicon, app title, remove CRA unnecessary remnants

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,5 @@
+// NOTE: the file must remain named setupTests.ts
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
