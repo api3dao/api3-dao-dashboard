69b4169956072471b9e99a642cea38e4c2b9e781
André Ogle <ogleandre@gmail.com>
2021-05-21 15:16:40 +0200

    Close modal on events

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
+import { useChainData } from '../../chain-data';
 import './modal.scss';
 
 interface ModalProps {
@@ -14,8 +15,18 @@ interface ModalProps {
 
 const Modal = (props: ModalProps) => {
   const { onClose, open, hideCloseButton, children, header, footer } = props;
+  const { provider, web3Modal } = useChainData();
+
+  useEffect(() => {
+    if (!provider || !web3Modal || !open) return;
+    web3Modal.on('accountsChanged', onClose);
+    return () => {
+      web3Modal.off('accountsChanged', onClose);
+    };
+  }, [provider, web3Modal]);
 
   if (!open) return null;
+
   return ReactDOM.createPortal(
     <div className="modal-wrapper">
       <div className="modal-body">
