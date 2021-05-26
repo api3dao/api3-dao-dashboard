af42d769c669b1196c45ed6ae2f54e17aa4749f0
André Ogle <ogleandre@gmail.com>
2021-05-26 13:24:44 +0200

    Remove usePromise hook

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 /**
  * Use this to bypass eslint rule checking if all hook dependencies are used.
@@ -9,39 +9,6 @@ import { useState, useEffect, useCallback, useRef } from 'react';
  */
 // TODO: verify that it is eliminated from production bundle
 export const unusedHookDependency = (..._args: any[]) => {};
-
-// Inspired by https://usehooks.com/useAsync/
-// Make sure the asyncFunction is wrapped memoized otherwise there will be
-// and infinite loop.
-
-// Intentionally avoiding external dependency to reduce the attack surface
-export const usePromise = <T, E = Error>(
-  asyncFunction: () => Promise<T>
-): [E | null, T | null, 'pending' | 'success' | 'error'] => {
-  const [status, setStatus] = useState<'pending' | 'success' | 'error'>('pending');
-  const [value, setValue] = useState<T | null>(null);
-  const [error, setError] = useState<E | null>(null);
-
-  const execute = useCallback(async () => {
-    setStatus('pending');
-    setError(null);
-
-    try {
-      setValue(await asyncFunction());
-      setStatus('success');
-    } catch (error) {
-      setError(error);
-      setStatus('error');
-    }
-  }, [asyncFunction]);
-
-  useEffect(() => {
-    execute();
-  }, [execute]);
-
-  // Promise value is returned second to remind developers to handle the error
-  return [error, value, status];
-};
 
 export const usePrevious = (value: any) => {
   const ref = useRef();
