7b295a65797052168c819be1b95c80335a31c614
André Ogle <ogleandre@gmail.com>
2021-05-28 13:58:03 +0200

    Move useOnAccountOrNetworkChange hook

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
-import { useOnAccountOrNetworkChange } from '../../utils';
+import { useOnAccountOrNetworkChange } from '../../contracts';
 import './modal.scss';
 
 interface ModalProps {
