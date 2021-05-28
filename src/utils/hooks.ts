2d83a00af62266225ee58dac8fd470b32bbce449
André Ogle <ogleandre@gmail.com>
2021-05-28 13:20:33 +0200

    Remove unusedHookDependency

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,16 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useChainData } from '../chain-data';
 
-/**
- * Use this to bypass eslint rule checking if all hook dependencies are used.
- * You might want to execute your hook on some value not directly
- * used in code (e.g. blockNumber)
- *
- * This function is noop, and will be removed from production bundle.
- */
-// TODO: verify that it is eliminated from production bundle
-export const unusedHookDependency = (..._args: any[]) => {};
-
 // Allows access to the previous to the previous value when re-rendering
 // https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
 export const usePrevious = (value: any) => {
