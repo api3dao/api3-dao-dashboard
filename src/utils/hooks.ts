6e96f5343a0a8af5c55d43bc299728e90ca12298
André Ogle <ogleandre@gmail.com>
2021-05-28 13:57:29 +0200

    Move useOnAccountOrNetworkChange hook

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react';
-import { useChainData } from '../chain-data';
 
 // Allows access to the previous to the previous value when re-rendering
 // https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
@@ -12,20 +11,4 @@ export const usePrevious = (value: any) => {
 
   // Return previous value (happens before update in useEffect above)
   return ref.current;
-};
-
-/**
- * Use this hook to have a function called when either the network or selected
- * account is changed
- */
-export const useOnAccountOrNetworkChange = (callback: () => any) => {
-  const { networkName, userAccount } = useChainData();
-  const prevUserAccount = usePrevious(userAccount);
-  const prevNetworkName = usePrevious(networkName);
-
-  useEffect(() => {
-    if (prevUserAccount !== userAccount || prevNetworkName !== networkName) {
-      callback();
-    }
-  }, [prevUserAccount, userAccount, prevNetworkName, networkName, callback]);
 };
