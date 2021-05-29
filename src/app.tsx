a6e836da4e3ee3011f40d0d24dfaa772a752752d
André Ogle <ogleandre@gmail.com>
2021-05-29 11:30:52 +0200

    Add notistack

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import './styles/main.scss';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
 import ChainDataContextProvider from './chain-data';
 import Dashboard from './pages/dashboard';
 import Proposals from './pages/proposals';
@@ -7,25 +8,27 @@ import ProposalDetails from './pages/proposal-details';
 
 function App() {
   return (
-    <ChainDataContextProvider>
-      <Router>
-        {/* NOTE: The first matching root is used. This imples that `/` must be last */}
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/proposals/:typeAndId">
-            <ProposalDetails />
-          </Route>
-          <Route path="/proposals">
-            <Proposals />
-          </Route>
-          <Route path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
-    </ChainDataContextProvider>
+    <SnackbarProvider maxSnack={5}>
+      <ChainDataContextProvider>
+        <Router>
+          {/* NOTE: The first matching root is used. This imples that `/` must be last */}
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/proposals/:typeAndId">
+              <ProposalDetails />
+            </Route>
+            <Route path="/proposals">
+              <Proposals />
+            </Route>
+            <Route path="/">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </Router>
+      </ChainDataContextProvider>
+    </SnackbarProvider>
   );
 }
 
