4faff2a1f2ecabebd85a3ecab5c4602bdee39bd3
André Ogle <ogleandre@gmail.com>
2021-06-07 14:56:27 +0200

    Remove Sentry tracing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import './index.scss';
 import App from './app';
 
 if (process.env.REACT_APP_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
   });
 }
 
