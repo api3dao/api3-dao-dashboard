faf7bd8a9ae3492b4695eee566e234fd6459e3ce
André Ogle <ogleandre@gmail.com>
2021-06-12 16:58:39 +0200

    Extract Sentry and ToastContainer to the PageContainer component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,4 @@
-import * as Sentry from '@sentry/react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import ChainDataContextProvider from './chain-data';
 import PageContainer from './pages/page-container';
 import Dashboard from './pages/dashboard';
@@ -11,43 +9,38 @@ import History from './pages/history';
 import './styles/variables.module.scss';
 
 function App() {
-  // TODO: Implement a nicely designed error page/component for Sentry.ErrorBoundary fallback
-  // See: https://docs.sentry.io/platforms/javascript/guides/react/components/errorboundary/
   return (
-    <Sentry.ErrorBoundary fallback="An error has occurred. Please refresh and try again.">
-      <ChainDataContextProvider>
-        <ToastContainer />
-        <PageContainer>
-          <Router>
-            <Switch>
-              <Route path="/dashboard" exact>
-                <Dashboard />
-              </Route>
-              <Route path="/proposals/:typeAndId" exact>
-                <ProposalDetails />
-              </Route>
-              <Route path="/proposals" exact>
-                <Proposals />
-              </Route>
-              <Route path="/history/:typeAndId" exact>
-                <HistoryDetails />
-              </Route>
-              <Route path="/history" exact>
-                <History />
-              </Route>
-              <Route path="/" exact>
-                <Dashboard />
-              </Route>
-              {/* NOTE: This fallback route must be last */}
-              <Route path="/">
-                {/* TODO: Not found page */}
-                <h5 style={{ color: 'black' }}>Not found</h5>
-              </Route>
-            </Switch>
-          </Router>
-        </PageContainer>
-      </ChainDataContextProvider>
-    </Sentry.ErrorBoundary>
+    <ChainDataContextProvider>
+      <PageContainer>
+        <Router>
+          <Switch>
+            <Route path="/dashboard" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/proposals/:typeAndId" exact>
+              <ProposalDetails />
+            </Route>
+            <Route path="/proposals" exact>
+              <Proposals />
+            </Route>
+            <Route path="/history/:typeAndId" exact>
+              <HistoryDetails />
+            </Route>
+            <Route path="/history" exact>
+              <History />
+            </Route>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            {/* NOTE: This fallback route must be last */}
+            <Route path="/">
+              {/* TODO: Not found page */}
+              <h5 style={{ color: 'black' }}>Not found</h5>
+            </Route>
+          </Switch>
+        </Router>
+      </PageContainer>
+    </ChainDataContextProvider>
   );
 }
 
--- src/pages/page-container.tsx
+++ src/pages/page-container.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/react';
+import { ToastContainer } from 'react-toastify';
 import { useTransactionNotifications } from '../contracts';
 
 interface Props {
@@ -9,7 +11,14 @@ const PageContainer = (props: Props) => {
   // Whenever a new transaction is added to the state, display notifications for it
   useTransactionNotifications();
 
-  return props.children;
+  // TODO: Implement a nicely designed error page/component for Sentry.ErrorBoundary fallback
+  // See: https://docs.sentry.io/platforms/javascript/guides/react/components/errorboundary/
+  return (
+    <Sentry.ErrorBoundary fallback="An error has occurred. Please refresh and try again.">
+      <ToastContainer />
+      props.children;
+    </Sentry.ErrorBoundary>
+  );
 };
 
 export default PageContainer;
