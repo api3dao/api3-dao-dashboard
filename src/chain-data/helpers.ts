31c96ebfb574d4e766ad541a428fad6cc490a0ae
André Ogle <ogleandre@gmail.com>
2021-06-14 18:27:11 +0200

    Update ethers to 5.3.1. Correctly show success/error toasts

--- src/chain-data/helpers.ts
+++ src/chain-data/helpers.ts
@@ -35,6 +35,12 @@ export const abbrStr = (str: string) => {
   return str.substr(0, 9) + '...' + str.substr(str.length - 4, str.length);
 };
 
+export const getEtherscanUrl = (hash: string) => {
+  // Non-mainnet networks have different Etherscan subdomains
+  const etherscanHost = process.env.REACT_APP_ETHERSCAN_HOST || 'https://etherscan.io';
+  return `${etherscanHost}/tx/${hash}`;
+};
+
 export interface PendingTransactionMessages {
   start: string;
   success: string;
@@ -45,33 +51,55 @@ export const displayPendingTransaction = async (
   transaction: ethers.ContractTransaction,
   messages: PendingTransactionMessages
 ) => {
-  // Non-mainnet networks have different Etherscan subdomains
-  const etherscanHost = process.env.REACT_APP_ETHERSCAN_HOST || 'https://etherscan.io';
-  const url = `${etherscanHost}/tx/${transaction.hash}`;
+  const url = getEtherscanUrl(transaction.hash);
 
   // It's common for transactions to take between 1-5 minutes to confirm. Keep the
   // initial "progress" toast open until then
-  const infoToastId = notifications.info({ url, message: messages.success }, { autoClose: false });
+  const infoToastId = notifications.info({ url, message: messages.start }, { autoClose: false, closeOnClick: false });
 
-  // NOTE: ethers.js adds various additional fields to Error, so it's easier to type as 'any'
-  // https://docs.ethers.io/v5/api/providers/types/#providers-TransactionRequest
-  const [err] = (await go(transaction.wait())) as any;
+  const [err, receipt] = await go(transaction.wait());
 
   if (infoToastId) notifications.close(infoToastId);
 
-  if (err) {
-    // A receipt with status 0 means the transaction failed
-    if (err.receipt?.status.toString() === '0') {
+  // NOTE: ethers.js adds various additional fields to Error, so it's easier to type as 'any'
+  // https://docs.ethers.io/v5/api/providers/types/#providers-TransactionResponse
+  const ethersError = err as any;
+  if (ethersError) {
+    // Transactions can be "replaced" or "cancelled". We don't need to worry about "repriced"
+    // See: https://blog.ricmoo.com/highlights-ethers-js-may-2021-2826e858277d
+    if (ethersError.code === ethers.errors.TRANSACTION_REPLACED) {
+      // The user "cancelled" the transaction. i.e. it was resent with the same
+      // nonce, but higher gas price, value as 0 and data as 0x
+      if (ethersError.cancelled) {
+        notifications.success({ message: 'Transaction cancelled successfully' });
+        return;
+      }
+
+      // The user "sped up" their transaction by resending it with a higher gas price
+      if (ethersError.replacement && ethersError.replacement.hash) {
+        const replacementTxUrl = getEtherscanUrl(ethersError.replacement.hash);
+        notifications.success({ url: replacementTxUrl, message: messages.success });
+        return;
+      }
+
+      // A receipt with status 0 means the transaction failed
+      if (ethersError.receipt?.status === 0) {
+        notifications.error({ url, message: messages.error });
+        return;
+      }
+    }
+  }
+
+  if (receipt) {
+    // A receipt with status 0 means the transaction failed and 1 indicates success
+    if (receipt.status === 0) {
       notifications.error({ url, message: messages.error });
       return;
     }
 
-    // If the user resends a transaction with the same nonce and higher gas price
-    if (err.reason === 'replaced' && err.replacement) {
-      await displayPendingTransaction(err.replacement, messages);
+    if (receipt.status === 1) {
+      notifications.success({ url, message: messages.success });
       return;
     }
   }
-
-  notifications.success({ url, message: messages.success });
 };
