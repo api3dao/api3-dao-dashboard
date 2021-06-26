2b4cc6c6c3413fc3c51db833f039eb9701eac84d
André Ogle <ogleandre@gmail.com>
2021-06-26 15:38:57 +0200

    Filter out the user's IP address and user agent

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,18 @@ if (process.env.REACT_APP_SENTRY_DSN && process.env.REACT_APP_NODE_ENV) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     environment: process.env.REACT_APP_NODE_ENV,
+    integrations(integrations) {
+      return integrations.filter((integration) => {
+        // Integrations can be filtered out here
+        // See: https://docs.sentry.io/platforms/javascript/configuration/integrations/default/
+        return !['UserAgent'].includes(integration.name);
+      });
+    },
+    beforeSend(event) {
+      // Do not collect the user's IP address
+      event.user = { ip_address: '0.0.0.0' };
+      return event;
+    },
   });
 }
 
