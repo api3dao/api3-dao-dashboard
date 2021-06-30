70748d084351adcabd972a71ca2f701965c545f5
Emanuel Tesar <e.tesarr@gmail.com>
2021-06-30 11:06:32 +0200

    Improve and make error messages consistent

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,123 +1,122 @@
 import { PendingTransactionMessages, TransactionType } from '../chain-data';
 
 export const messages = {
-  // Dashboard errors
-  FAILED_TO_LOAD_DASHBOARD_DATA: 'Failed to load latest dashboard data',
-
   // Transaction messages
-  TX_APPROVAL_REJECTED: 'API3 token approval transaction rejected',
-  TX_APPROVAL_ERROR: 'Failed to approve API3 token allowance. Please try again',
-  TX_DEPOSIT_REJECTED: 'API3 token deposit transaction rejected',
-  TX_DEPOSIT_ERROR: 'Failed to deposit API3 tokens. Please try again',
-  TX_GENERIC_REJECTED: 'Transaction rejected',
-  TX_GENERIC_ERROR: 'An error has occurred. Please try again',
+  TX_APPROVAL_REJECTED: 'API3 token approval transaction rejected.',
+  TX_APPROVAL_ERROR: 'Failed to approve API3 token allowance. Please try again.',
+  TX_DEPOSIT_REJECTED: 'API3 token deposit transaction rejected.',
+  TX_DEPOSIT_ERROR: 'Failed to deposit API3 tokens. Please try again.',
+  TX_GENERIC_REJECTED: 'Transaction rejected.',
+  TX_GENERIC_ERROR: 'An error has occurred. Please try again.',
 
   // Validation messages
-  VALIDATION_INPUT_ZERO: 'Please ensure you have entered a positive, non-zero amount',
-  VALIDATION_INPUT_PARSE: 'Unable to parse input amount',
-  VALIDATION_DEPOSIT_TOO_HIGH: 'Deposit value cannot be higher than the available balance',
-  VALIDATION_INPUT_TOO_HIGH: 'Input amount cannot be higher than the available balance',
+  VALIDATION_INPUT_ZERO: 'Please enter a value greater than 0.',
+  VALIDATION_INPUT_PARSE: 'Failed to parse input amount.',
+  VALIDATION_DEPOSIT_TOO_HIGH: 'Value too large. Please enter a value equal to or less than your available balance.',
+  VALIDATION_INPUT_TOO_HIGH: 'Value too large. Please enter a value equal to or less than your available balance.',
 
   // Blockchain errors
-  FAILED_TO_LOAD_CHAIN_DATA: 'Unable to load blockchain data',
-  FAILED_TO_LOAD_TREASURY_AND_DELEGATION: 'Unable to load delegation and treasury data',
+  FAILED_TO_LOAD_CHAIN_DATA: 'Failed to load blockchain data. Please try again later.',
+  FAILED_TO_LOAD_TREASURY_AND_DELEGATION: 'Failed to load delegation and treasury data. Please try again later.',
 
   // Delegation errors
-  UNABLE_TO_LOAD_DELEGATE: 'Unable to load the delagate of address',
-  INVALID_DELEGATE_ADDRESS: 'Delegation target must be a valid non zero address',
-  DELEGATE_IS_YOURSELF: "You can't delegate to yourself",
-  REDELEGATION_IS_FORBIDDEN: (targetDelegate: string) => `Address ${targetDelegate} delegates to some other account`,
+  FAILED_TO_LOAD_DELEGATE:
+    'Failed to verify that the intended delegate address is not already delegated. Please try again later.',
+  INVALID_DELEGATE_ADDRESS: 'Please delegate to a valid, non-zero address.',
+  DELEGATE_IS_YOURSELF: 'Voting power cannot be delegated to your connected address.',
+  REDELEGATION_IS_FORBIDDEN: (targetDelegate: string) =>
+    `Failed to delegate to address ${targetDelegate} because it is currently delegated to a different address.`,
 
   // Proposals errors
-  FAILED_TO_LOAD_PROPOSALS: 'Unable to load proposals',
-  FAILED_TO_LOAD_GENESIS_EPOCH: 'Unable to load the current epoch',
-  INVALID_PROPOSAL_FORMAT: 'This proposal was not created through the DAO dashboard and cannot to be displayed',
-  PROPOSAL_NOT_FOUND: `Proposal not found`,
+  FAILED_TO_LOAD_PROPOSALS: 'Failed to load proposals. Please try again later.',
+  FAILED_TO_LOAD_GENESIS_EPOCH: 'Failed to load the current epoch.',
+  INVALID_PROPOSAL_FORMAT: 'This proposal was not created through the DAO dashboard and cannot to be displayed.',
+  PROPOSAL_NOT_FOUND: 'Proposal not found.',
 
   // Vesting errors
-  FAILED_TO_LOAD_VESTING_DATA: 'Failed to load vesting data',
+  FAILED_TO_LOAD_VESTING_DATA: 'Failed to load vesting data. Please try again later.',
 };
 
 // TODO: these messages should change depending on the final designs
 export const transactionMessages: { [key in TransactionType]: PendingTransactionMessages } = {
   'approve-deposit': {
     start: 'Approving API3 token allowance...',
-    success: 'API3 token allowance approved!',
-    error: 'API3 token allowance failed',
+    success: 'Success! API3 token allowance approved.',
+    error: 'API3 token allowance failed.',
   },
   'deposit-only': {
     start: 'Depositing API3 tokens...',
-    success: 'API3 tokens deposited successfully!',
-    error: 'API3 token deposit failed',
+    success: 'Success! API3 tokens deposited.',
+    error: 'Failed to deposit API3 tokens. Please try again.',
   },
   'deposit-and-stake': {
     start: 'Depositing and staking API3 tokens...',
-    success: 'API3 tokens deposited and staked successfully!',
-    error: 'API3 token deposit and stake failed',
+    success: 'Success! API3 tokens deposited and staked.',
+    error: 'Failed to deposit and stake API3 tokens. Please try again.',
   },
   stake: {
     start: 'Staking API3 tokens...',
-    success: 'API3 tokens staked successfully!',
-    error: 'API3 token stake failed',
+    success: 'Success! API3 tokens staked.',
+    error: 'Failed to stake API3 tokens. Please try again.',
   },
   'initiate-unstake': {
-    start: 'Initiating API3 token unstake...',
-    success: 'API3 token unstake initiated successfully!',
-    error: 'API3 token unstake initiation failed',
+    start: 'Initiating unstaking of API3 tokens...',
+    success: 'Success! Initiated unstaking of API3 tokens.',
+    error: 'Failed to initiate unstaking of API3 tokens. Please try again.',
   },
   unstake: {
     start: 'Unstaking API3 tokens...',
-    success: 'API3 tokens unstaked successfully!',
-    error: 'API3 token unstake failed',
+    success: 'Success! API3 tokens unstaked.',
+    error: 'Failed to unstake API3 tokens. Please try again.',
   },
   'unstake-withdraw': {
     start: 'Unstaking and withdrawing API3 tokens...',
-    success: 'API3 tokens unstaked and withdrawn successfully!',
-    error: 'API3 token unstake and withdraw failed',
+    success: 'Success! API3 tokens unstaked and withdrawn.',
+    error: 'Failed to unstake and withdraw API3 tokens. Please try again.',
   },
   withdraw: {
     start: 'Withdrawing API3 tokens...',
-    success: 'API3 tokens withdrawn successfully!',
-    error: 'API3 token withdraw failed',
+    success: 'Success! API3 tokens withdrawn.',
+    error: 'Failed to withdraw API3 tokens. Please try again.',
   },
   delegate: {
     start: 'Delegating voting power...',
-    success: 'Voting power delegated successfully!',
-    error: 'Voting power delegation failed',
+    success: 'Success! Voting power delegated.',
+    error: 'Failed to delegate voting power. Please try again.',
   },
   undelegate: {
     start: 'Undelegating voting power...',
-    success: 'Voting power undelegated successfully!',
-    error: 'Voting power undelegation failed',
+    success: 'Success! Voting power undelegated.',
+    error: 'Failed to undelegate voting power. Please try again.',
   },
   'new-vote': {
     start: 'Submitting new proposal...',
-    success: 'New proposal submitted successfully!',
-    error: 'New proposal submission failed',
+    success: 'Success! New proposal submitted.',
+    error: 'Failed to submit new proposal. Please try again.',
   },
   'vote-for': {
-    start: 'Voting for proposal...',
-    success: 'Vote for proposal cast successfully!',
-    error: 'Vote for proposal failed',
+    start: 'Casting vote “For” proposal...',
+    success: 'Success! Vote “For” has been cast.',
+    error: 'Failed to cast vote “For”. Please try again.',
   },
   'vote-against': {
-    start: 'Voting against proposal...',
-    success: 'Vote against proposal cast successfully!',
-    error: 'Vote against proposal failed',
+    start: 'Casting vote “Against” proposal...',
+    success: 'Success! Vote “Against” has been cast.',
+    error: 'Failed to cast vote “Against”. Please try again.',
   },
   execute: {
     start: 'Executing proposal...',
-    success: 'Successfully executed proposal!',
-    error: 'Proposal execution failed',
+    success: 'Success! Proposal executed.',
+    error: 'Failed to execute proposal. Please try again.',
   },
   'update-timelock-status': {
     start: 'Updating timelock status...',
-    success: 'Successfully updated timelock status!',
-    error: 'Timelock update failed',
+    success: 'Success! Timelock status updated.',
+    error: 'Failed to update timelock status. Please try again.',
   },
   'withdraw-to-pool': {
     start: 'Withdrawing to pool...',
-    success: 'Successfully withdrawn to pool!',
-    error: 'Withdrawal to pool failed',
+    success: 'Success! API3 tokens withdrawn to pool.',
+    error: 'Failed to withdraw to pool. Please try again.',
   },
 };
