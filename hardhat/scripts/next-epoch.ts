fc6fc6e339590034efb00f6d4024a711a6d151c5
André Ogle <andreogle@users.noreply.github.com>
2021-08-09 11:49:27 +0200

    Force a new block after increasing the time (#218)

--- hardhat/scripts/next-epoch.ts
+++ hardhat/scripts/next-epoch.ts
@@ -4,6 +4,9 @@ import { EPOCH_LENGTH } from '../../src/contracts/helpers';
 async function main() {
   await ethers.provider.send('evm_increaseTime', [EPOCH_LENGTH]); // move the time to the next epoch
   console.info(`NOTE: You may need to reset your metamask wallet after the command completes`);
+
+  // Force a new block as the 'isGenesisEpoch' check depends on the timestamp of the current block
+  await ethers.provider.send('evm_mine', []);
 }
 
 main()
