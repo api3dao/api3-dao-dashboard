70045c045ef9dbab4d0c61f92cedced2eddda583
Emanuel Tesar <e.tesarr@gmail.com>
2021-08-15 10:22:37 +0200

    Fix correct staking calculation, add clarification tooltips

--- src/pages/dashboard/staking/staking-pool.tsx
+++ src/pages/dashboard/staking/staking-pool.tsx
@@ -5,57 +5,67 @@ import RadialChart from './radial-chart';
 import globalStyles from '../../../styles/global-styles.module.scss';
 import styles from './staking-pool.module.scss';
 import classNames from 'classnames';
-import { UNKNOWN_NUMBER } from '../../../utils';
+import { images, UNKNOWN_NUMBER } from '../../../utils';
+import { Tooltip } from '../../../components/tooltip';
 
 const StakingPool = () => {
   const { dashboardState: data } = useChainData();
   const stakingPool = stakingPoolSelector(data);
 
   const currentApy = stakingPool ? round(stakingPool.currentApy, 1) : UNKNOWN_NUMBER;
-  const annualInflationRate = stakingPool ? round(stakingPool.annualInflationRate) : UNKNOWN_NUMBER;
+  const annualTotalSupplyGrowth = stakingPool ? round(stakingPool.annualTotalSupplyGrowth) : UNKNOWN_NUMBER;
   const stakedPercentage = stakingPool ? Number.parseFloat(round(stakingPool.stakedPercentage)) : undefined;
   const totalStaked = data ? formatAndRoundApi3(data.totalStake) : UNKNOWN_NUMBER;
   const stakingTargetInTokens = stakingPool ? formatAndRoundApi3(stakingPool.stakingTargetInTokens) : UNKNOWN_NUMBER;
 
   const currentApyText = currentApy === UNKNOWN_NUMBER ? currentApy : `${currentApy}%`;
-  const annualInflationRateText =
-    annualInflationRate === UNKNOWN_NUMBER ? annualInflationRate : `${annualInflationRate}%`;
+  const annualTotalSupplyGrowthText =
+    annualTotalSupplyGrowth === UNKNOWN_NUMBER ? annualTotalSupplyGrowth : `${annualTotalSupplyGrowth}%`;
+  const estimatedValueTooltip = 'Value is based on the current APR and will change in time';
 
   return (
     <div className={styles.stakingPool}>
-      <div className={classNames(styles.stakingTable, styles.twoCells)}>
+      <div className={styles.firstSection}>
         <div className={styles.stakingTableRow}>
           <div className={classNames(styles.stakingTableCell, styles.rightAlign)}>
             <p className={styles.stakingAnnualApy}>{currentApyText}</p>
           </div>
           <div className={styles.stakingTableCell}>
             <p className={globalStyles.secondaryColor}>Annual Rewards (APY)</p>
+            <Tooltip overlay={estimatedValueTooltip}>
+              <img src={images.help} alt="help" className={globalStyles.helpIcon} />
+            </Tooltip>
           </div>
         </div>
         <div className={styles.stakingTableRow}>
           <div className={classNames(styles.stakingTableCell, styles.rightAlign)}>
-            <h5>{annualInflationRateText}</h5>
+            <h5>{annualTotalSupplyGrowthText}</h5>
           </div>
           <div className={styles.stakingTableCell}>
-            <p className={globalStyles.secondaryColor}>Annual Inflation Rate</p>
+            <p className={globalStyles.secondaryColor}>Annual total supply growth</p>
+            <Tooltip overlay={estimatedValueTooltip}>
+              <img src={images.help} alt="help" className={globalStyles.helpIcon} />
+            </Tooltip>
           </div>
         </div>
       </div>
-      <div className={styles.stakingTableWrap}>
-        <div className={classNames(styles.stakingTable, styles.oneCell)}>
+
+      <div className={classNames(styles.reverseDirectionWrap)}>
+        <div className={styles.secondSection}>
           <div className={styles.stakingTableRow}>
-            <div className={classNames(styles.stakingTableCell, styles.oneCell, styles.rightAlign)}>
+            <div className={classNames(styles.stakingTableCell, styles.rightAlign)}>
               <p className={styles.stakingTableCellTitle}>total staked</p>
               <p className={globalStyles.medium}>{totalStaked}</p>
             </div>
           </div>
           <div className={styles.stakingTableRow}>
-            <div className={classNames(styles.stakingTableCell, styles.oneCell, styles.rightAlign)}>
+            <div className={classNames(styles.stakingTableCell, styles.rightAlign)}>
               <p className={styles.stakingTableCellTitle}>staking target</p>
               <p className={globalStyles.medium}>{stakingTargetInTokens}</p>
             </div>
           </div>
         </div>
+
         <div className={styles.stakingRadialChart}>
           <RadialChart completionPercent={stakedPercentage} />
         </div>
