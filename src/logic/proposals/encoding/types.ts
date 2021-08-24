a8ac7a388bad1ecf5229bde00ec10326be2833a9
Emanuel Tesar <e.tesarr@gmail.com>
2021-08-24 06:32:11 +0200

    Move and slightly change generic DAO contracts comment

--- src/logic/proposals/encoding/types.ts
+++ src/logic/proposals/encoding/types.ts
@@ -1,10 +1,5 @@
 import { ProposalType } from '../../../chain-data';
 
-/**
- * NOTE: Aragon contracts are flexible but this makes it a bit harder to work with it's contracts. We have created a
- * simple encoding/decoding scheme for the API3 proposals. The implementation of these utilities is inspired by
- * https://github.com/bbenligiray/proposal-test.
- */
 export interface NewProposalFormData {
   type: ProposalType;
   title: string;
