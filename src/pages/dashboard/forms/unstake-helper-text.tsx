2efe6f298a8e2382da6873d4c49c4569087d7607
Emanuel Tesar <e.tesarr@gmail.com>
2022-01-03 12:22:45 +0100

    Fix typo in unstake helper text

--- src/pages/dashboard/forms/unstake-helper-text.tsx
+++ src/pages/dashboard/forms/unstake-helper-text.tsx
@@ -3,7 +3,7 @@ import styles from './forms.module.scss';
 const messages = {
   basic: 'Once staked, you will not be able to unstake your tokens for 7 days.',
   extended:
-    'Unstaked tokens will no longer grant you rewards or voting power. Your will not be able to withdraw your unstaked tokens for 7 days.',
+    'Unstaked tokens will no longer grant you rewards or voting power. You will not be able to withdraw your unstaked tokens for 7 days.',
 };
 
 interface UnstakeHelperTextProps {
