5de1faec83869e9c57cb4b0508a1a772063e3350
Markus Coetzee <mks.coetzee@gmail.com>
2022-06-15 15:39:29 +0200

    DAO-157 Add search input that filters by IPFS hash

--- src/pages/policies/policies.tsx
+++ src/pages/policies/policies.tsx
@@ -1,34 +1,42 @@
-import { ReactNode, useMemo } from 'react';
+import { FormEventHandler, ReactNode, useMemo } from 'react';
 import { useHistory } from 'react-router';
 import Layout from '../../components/layout';
 import BorderedBox, { Header } from '../../components/bordered-box';
 import RadioButton from '../../components/radio-button';
 import Button from '../../components/button';
+import Input from '../../components/input';
+import CloseIcon from '../../components/icons/close-icon';
+import SearchIcon from '../../components/icons/search-icon';
 import PolicyList from './policy-list';
 import { useQueryParams } from '../../utils';
 import { connectWallet } from '../../components/sign-in/sign-in';
 import { useChainData } from '../../chain-data';
 import { useUserPolicies, isActive } from '../../logic/policies';
-import styles from '../claims/claims.module.scss';
+import styles from './policies.module.scss';
 
 export default function Policies() {
   const { data: policies, status } = useUserPolicies();
 
   const params = useQueryParams();
+  const query = params.get('ipfs-hash') || '';
   const filter = params.get('filter');
   const filteredPolicies = useMemo(() => {
-    if (!policies) return [];
-    switch (filter) {
-      case 'none':
-        return [];
-      case 'active':
-        return policies.filter((policy) => isActive(policy));
-      case 'inactive':
-        return policies.filter((policy) => !isActive(policy));
-      default:
-        return policies;
+    if (!policies || filter === 'none') return [];
+
+    let results = policies;
+    if (query) {
+      results = results.filter((policy) => policy.ipfsHash.toLowerCase().includes(query.toLowerCase()));
     }
-  }, [policies, filter]);
+
+    switch (filter) {
+      case 'active':
+        return results.filter((policy) => isActive(policy));
+      case 'inactive':
+        return results.filter((policy) => !isActive(policy));
+      default:
+        return results;
+    }
+  }, [policies, query, filter]);
 
   const { provider, setChainData } = useChainData();
   if (!provider) {
@@ -55,11 +63,19 @@ export default function Policies() {
   return (
     <PoliciesLayout>
       {filteredPolicies.length > 0 ? (
-        <PolicyList policies={filteredPolicies} />
+        <>
+          <div className={styles.resultsInfo}>
+            {filteredPolicies.length} result(s)
+            {query && <> for "{query}"</>}
+          </div>
+          <PolicyList policies={filteredPolicies} />
+        </>
       ) : policies.length === 0 ? (
         <p className={styles.emptyState}>There are no policies linked to your account.</p>
       ) : (
-        <p className={styles.emptyState}>There are no matching policies.</p>
+        <p className={styles.emptyState}>
+          {query ? <>There are no policies matching "{query}"</> : <>There are no matching policies.</>}
+        </p>
       )}
     </PoliciesLayout>
   );
@@ -70,25 +86,69 @@ interface PoliciesLayoutProps {
 }
 
 function PoliciesLayout(props: PoliciesLayoutProps) {
+  const params = useQueryParams();
   const history = useHistory();
+
   const handleFilterChange = (showActive: boolean, showInactive: boolean) => {
+    const newParams = new URLSearchParams(params);
     if (showActive && !showInactive) {
-      history.replace(`/policies?filter=active`);
+      newParams.set('filter', 'active');
     } else if (!showActive && showInactive) {
-      history.replace(`/policies?filter=inactive`);
+      newParams.set('filter', 'inactive');
     } else if (!showActive && !showInactive) {
-      history.replace(`/policies?filter=none`);
+      newParams.set('filter', 'none');
     } else {
-      history.replace(`/policies`);
+      newParams.delete('filter');
     }
+    history.replace('/policies?' + newParams.toString());
   };
 
-  const params = useQueryParams();
   const filter = params.get('filter');
   const activeChecked = !filter || filter === 'active';
   const inactiveChecked = !filter || filter === 'inactive';
+
+  const query = params.get('ipfs-hash') || '';
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault();
+    const { value } = ev.currentTarget.query;
+    if (value) {
+      history.replace('/policies?ipfs-hash=' + value);
+    } else if (query) {
+      history.replace('/policies');
+    }
+  };
+
+  const handleClear = () => {
+    const newParams = new URLSearchParams(params);
+    newParams.delete('ipfs-hash');
+    history.replace('/policies?' + newParams.toString());
+  };
+
   return (
     <Layout title="Policies">
+      <form className={styles.searchForm} onSubmit={handleSubmit}>
+        <button type="submit">
+          <SearchIcon aria-hidden />
+          <span className="sr-only">Submit</span>
+        </button>
+        <div className={styles.inputContainer}>
+          <Input
+            key={query}
+            name="query"
+            defaultValue={query}
+            aria-label="Search by IPFS hash"
+            placeholder="Search by IPFS hash"
+            underline={false}
+            block
+          />
+        </div>
+        {query && (
+          <button tabIndex={-1} type="button" className={styles.clearButton} onClick={handleClear}>
+            <CloseIcon aria-hidden />
+            <span className="sr-only">Clear</span>
+          </button>
+        )}
+      </form>
       <BorderedBox
         noMobileBorders
         header={
