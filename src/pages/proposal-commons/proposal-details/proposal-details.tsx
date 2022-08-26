1318cb7c61233d535e0a5e9225d72a68ffe7aee3
Markus Coetzee <mks.coetzee@gmail.com>
2022-08-26 10:05:28 +0200

    DAO-194 Show Wei amount on Proposal Details page

--- src/pages/proposal-commons/proposal-details/proposal-details.tsx
+++ src/pages/proposal-commons/proposal-details/proposal-details.tsx
@@ -1,4 +1,4 @@
-import { BigNumber, utils } from 'ethers';
+import { BigNumber } from 'ethers';
 import { ComponentProps, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -248,7 +248,7 @@ const ProposalDetailsContent = (props: ProposalDetailsProps) => {
             {value.gt(0) && (
               <div className={styles.proposalDetailsItem}>
                 <p className={globalStyles.bold}>ETH Value</p>
-                <p className={globalStyles.secondaryColor}>{utils.formatEther(value)}</p>
+                <p className={globalStyles.secondaryColor}>{value.toString()}</p>
               </div>
             )}
             <div className={styles.proposalDetailsItem}>
