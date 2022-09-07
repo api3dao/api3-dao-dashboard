888adde0c15096002d5f9905aed93a1c67031aa1
Markus Coetzee <mks.coetzee@gmail.com>
2022-09-07 09:57:10 +0200

    DAO-188 Use camelCase in session storage and history state

--- src/components/back-button/back-button.tsx
+++ src/components/back-button/back-button.tsx
@@ -14,7 +14,7 @@ export default function BackButton(props: Props) {
   const { fallback, children } = props;
 
   // We can go back if we are not on the first page the user visited
-  const canGoBack = !window.history.state?._app_entry_page;
+  const canGoBack = !window.history.state?.appEntryPage;
 
   if (!canGoBack && fallback) {
     return (
@@ -39,9 +39,9 @@ export default function BackButton(props: Props) {
 export function identifyAppEntryPage() {
   // We use session storage so that if the user refreshes the browser tab, that we don't
   // identify another entry page
-  if (window.sessionStorage.getItem('has_identified_app_entry_page') !== 'true') {
+  if (window.sessionStorage.getItem('hasIdentifiedAppEntryPage') !== 'true') {
     // The HashRouter (from react-router) doesn't support history/location state: https://v5.reactrouter.com/web/api/HashRouter
-    window.history.replaceState({ _app_entry_page: true }, '');
-    window.sessionStorage.setItem('has_identified_app_entry_page', 'true');
+    window.history.replaceState({ appEntryPage: true }, '');
+    window.sessionStorage.setItem('hasIdentifiedAppEntryPage', 'true');
   }
 }
