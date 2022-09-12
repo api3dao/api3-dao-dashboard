f8fa55de7138c73ca68c87bf428561df3f56e71a
Markus Coetzee <mks.coetzee@gmail.com>
2022-09-12 11:49:59 +0200

    DAO-207 Prevent overwriting an existing localhost-dao.json file in postinstall script

--- dev-scripts/copy-localhost-data.js
+++ dev-scripts/copy-localhost-data.js
@@ -1,11 +1,25 @@
 /**
  * For localhost development, we depend on `localhost-dao.json` file which is gitignored because it will change after
- * each redeployment of the DAO contracts. However, we have to make sure it is created when before build the
+ * each redeployment of the DAO contracts. However, we have to make sure it is created before we build the
  * application.
  */
+const { join } = require('path');
+const { existsSync } = require('fs');
+const isEqual = require('lodash/isEqual');
 const { promiseWrapper, execAndLog } = require('./utils');
+const exampleAddresses = require('../src/contract-deployments/localhost-dao.example.json');
 
 const main = async () => {
+  const localAddressesPath = join(__dirname, '../src/contract-deployments/localhost-dao.json');
+  if (existsSync(localAddressesPath)) {
+    // We prevent overwriting an existing localhost-dao.json file if its keys (i.e. the contract names)
+    // matches the example file's keys.
+    const localAddresses = require(localAddressesPath);
+    if (isEqual(Object.keys(localAddresses), Object.keys(exampleAddresses))) {
+      return;
+    }
+  }
+
   await execAndLog('cd src/contract-deployments && cp localhost-dao.example.json localhost-dao.json');
 };
 
