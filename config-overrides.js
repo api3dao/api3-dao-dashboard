1b0c7f9f178c823efe027f1f452fd8ed07a5f4e9
Markus Coetzee <mks.coetzee@gmail.com>
2022-12-20 10:18:08 +0200

    Add a comment for the optimization

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,14 @@
-// See: https://www.alchemy.com/blog/how-to-polyfill-node-core-modules-in-webpack-5
 const webpack = require('webpack');
 
-module.exports = function override(config, webpackEnv) {
-  if (webpackEnv === 'production') {
+module.exports = function override(config, env) {
+  if (env === 'production') {
+    // Webpack produces a single JS bundle with the default configuration that react-scripts v5 provides,
+    // so we specify this optimization to get 2 bundles (like in react-scripts v4).
+    // See: https://github.com/facebook/create-react-app/discussions/11278#discussioncomment-1808511
     config.optimization.splitChunks = { chunks: 'all' };
   }
 
+  // See: https://www.alchemy.com/blog/how-to-polyfill-node-core-modules-in-webpack-5
   config.resolve.fallback = {
     ...config.resolve.fallback,
     http: require.resolve('stream-http'),
@@ -20,16 +23,15 @@ module.exports = function override(config, webpackEnv) {
     }),
   ]);
 
-  // See: https://github.com/facebook/create-react-app/pull/11752#issuecomment-1137238078
   config.ignoreWarnings = [
-    // Ignore warnings raised by source-map-loader. Some third party packages may ship miss-configured sourcemaps,
-    // that interrupts the build. See: https://github.com/facebook/create-react-app/discussions/11278#discussioncomment-1780169
+    // Ignore warnings raised by source-map-loader for third party packages.
+    // See: https://github.com/facebook/create-react-app/pull/11752#issuecomment-1137238078
     /**
      *
      * @param {import('webpack').WebpackError} warning
      * @returns {boolean}
      */
-    function ignoreSourcemapsloaderWarnings(warning) {
+    function ignoreSourceMapLoaderWarnings(warning) {
       return (
         warning.module &&
         warning.module.resource.includes('node_modules') &&
