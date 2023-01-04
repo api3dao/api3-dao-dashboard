798f9a6fc9a002b4538336be9a4e28f737f1e33f
Markus Coetzee <mks.coetzee@gmail.com>
2023-01-04 14:36:44 +0200

    Add comment about why strict mode was turned off

--- src/index.tsx
+++ src/index.tsx
@@ -31,4 +31,7 @@ if (process.env.REACT_APP_NODE_ENV === 'development' && (window as any).ethereum
 
 const root = createRoot(document.getElementById('root')!);
 
+// NOTE: Strict mode triggers useEffect and useMemo hooks twice on mount (in development), which results
+// in our data hooks triggering 3 times on mount (the third trigger is caused by the memoized smart contracts
+// which are included in the useEffect dependency arrays). Because of this, we have decided to turn off strict mode.
 root.render(<App />);
