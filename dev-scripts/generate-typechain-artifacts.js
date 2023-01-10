3843a8bbdf095018639b54957afb635865a29655
Markus Coetzee <mks.coetzee@gmail.com>
2023-01-10 12:21:57 +0200

    Clean up old artifacts folder

--- dev-scripts/generate-typechain-artifacts.js
+++ dev-scripts/generate-typechain-artifacts.js
@@ -1,3 +1,5 @@
+const { join } = require('path');
+const { existsSync } = require('fs');
 const { bold, green } = require('chalk');
 const { execAndLog, promiseWrapper } = require('./utils');
 
@@ -6,11 +8,16 @@ const DEBUG = false;
 
 const main = async () => {
   console.info(bold(green('Generating typechain artifacts...')));
-
   await execAndLog(
     "yarn typechain --target ethers-v5 --out-dir ./src/contracts/artifacts './src/contracts/abi/**/!(*.dbg).json'",
     DEBUG
   );
+
+  const oldArtifactsDir = join(__dirname, '../src/generated-contracts');
+  if (existsSync(oldArtifactsDir)) {
+    console.info(bold(green('Removing old typechain artifacts directory...')));
+    await execAndLog(`rm -rf ${oldArtifactsDir}`, DEBUG);
+  }
 };
 
 promiseWrapper(main);
