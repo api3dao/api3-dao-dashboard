89b1db163b889720d4324127af6ebb2ec4f1d14b
Emanuel Tesař <e.tesarr@gmail.com>
2023-11-04 10:41:00 +0100

    Upgrade to web3modal v3 and avoid using public RPCs

--- src/components/connect-button/connect-button.tsx
+++ src/components/connect-button/connect-button.tsx
@@ -1,4 +1,4 @@
-import { useWeb3Modal } from '@web3modal/react';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 import Button, { Props } from '../button';
 
 export default function ConnectButton(props: Props) {
