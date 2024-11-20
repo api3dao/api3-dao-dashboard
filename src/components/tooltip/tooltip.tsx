18117590673e20aed8adaca3629711d455e155bf
Bogdan Iasinovschi <biasinovschi@gmail.com>
2024-11-20 16:11:25 +0200

    Rebrand tooltips (#488)
    * Update default tooltip
    
    * Update items tooltip
    
    * Fix staking pool table tooltips offset
    
    * Fix unchecked style

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -1,32 +1,38 @@
 import { ReactElement } from 'react';
 import RCTooltip from 'rc-tooltip';
-// Additional styles that require access to CSS variables
-import styles from './tooltip.module.scss';
-// NOTE: This is required for basic tooltip styling
-import 'rc-tooltip/assets/bootstrap_white.css';
+import { useWindowDimensions } from '../../hooks/use-window-dimensions';
+import classNames from 'classnames';
+import styles from './tooltip.module.scss'; // Additional styles that require access to CSS variables
+
+import 'rc-tooltip/assets/bootstrap_white.css'; // NOTE: This is required for basic tooltip styling
+import './bootstrap_white.css'; // Override basic tooltip styling
 
 type Props = {
   children: ReactElement;
   overlay: ReactElement | string;
+  type?: 'default' | 'items' | 'full';
 };
 
-const Tooltip = ({ children, overlay }: Props) => {
+const Tooltip = ({ children, overlay, type = 'default' }: Props) => {
   // NOTE: rc-tooltip requires us to override default styles directly using objects
   // https://github.com/react-component/tooltip#props
   const overlayInnerStyle = {
-    alignItems: 'center',
-    background: 'linear-gradient(76.31deg, #f3f3f3 36.47%, #c3c4c3 99.02%)',
     display: 'flex',
+    alignItems: 'center',
+    background: '#0C1143',
   };
+
+  const { isMobile } = useWindowDimensions();
 
   // NOTE: rc-tooltip can be debugged by setting a 'visible' (boolean) prop
   return (
     <RCTooltip
       overlay={<div className={styles.overlayWrapper}>{overlay}</div>}
-      placement="bottom"
-      overlayClassName={styles.tooltip}
+      placement="bottomRight"
+      overlayClassName={classNames([styles[`${type}TooltipType`]])}
       overlayInnerStyle={overlayInnerStyle}
       mouseEnterDelay={0.2}
+      align={{ offset: isMobile ? [4, 6] : [18, 6] }}
     >
       {children}
     </RCTooltip>
