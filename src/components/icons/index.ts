76c0e67346eb74b5e9c3ac2db1141cd669834737
Bogdan Iasinovschi <bogdan.iasinovschi@vacuumlabs.com>
2024-12-03 13:00:08 +0200

    Update Vot on proposal modal

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -4,5 +4,16 @@ import { CloseIcon } from './close';
 import { CrossIcon } from './cross';
 import { HelpOutlineIcon } from './help-outline';
 import { InfoCircleIcon } from './info-circle';
+import { RadioButtonIcon } from './radio-button';
+import { RadioButtonFillIcon } from './radio-button-fill';
 
-export { CheckCircleFillIcon, CheckboxRadioIcon, CloseIcon, CrossIcon, HelpOutlineIcon, InfoCircleIcon };
+export {
+  CheckCircleFillIcon,
+  CheckboxRadioIcon,
+  CloseIcon,
+  CrossIcon,
+  HelpOutlineIcon,
+  InfoCircleIcon,
+  RadioButtonIcon,
+  RadioButtonFillIcon,
+};
--- src/components/radio-button/radio-button.tsx
+++ src/components/radio-button/radio-button.tsx
@@ -2,18 +2,48 @@ import { ReactNode } from 'react';
 import classNames from 'classnames';
 import styles from './radio-button.module.scss';
 import { triggerOnEnter } from '../modal';
-import { CheckboxRadioIcon, CheckCircleFillIcon } from '../icons';
+import { CheckboxRadioIcon, CheckCircleFillIcon, RadioButtonFillIcon, RadioButtonIcon } from '../icons';
+
+type Radio = { type: 'radio'; size?: 'default'; color?: undefined };
+type RadioLarge = { type?: 'radio'; size: 'large'; color?: 'default' | 'success' | 'warning' };
+type Checkbox = { type?: 'checkbox'; size?: 'default'; color?: undefined };
 
 type Props = {
   checked: boolean;
   children: ReactNode;
   name?: string;
-  type?: 'radio' | 'checkbox';
   onChange: () => void;
-};
+} & (Radio | RadioLarge | Checkbox);
 
 const RadioButton = (props: Props) => {
-  const { checked, children, name, type = 'radio', onChange } = props;
+  const { checked, children, name, type = 'radio', size = 'default', color = 'default', onChange } = props;
+
+  if (size === 'large') {
+    return (
+      <div role={type} aria-checked={checked} tabIndex={0} onKeyDown={triggerOnEnter(onChange)}>
+        <label
+          className={classNames(styles.radioButtonLabel, styles.radioButtonLabelLarge, [styles[color]], {
+            [styles.checked]: checked,
+          })}
+        >
+          {children}
+
+          <input
+            className={styles.radioButtonInput}
+            type={type}
+            name={name}
+            tabIndex={-1}
+            onChange={onChange}
+            checked={checked}
+          />
+
+          <span className={classNames(styles.radioButtonIcon, styles.radioButtonCircle, [styles[color]])}>
+            {checked ? <RadioButtonFillIcon /> : <RadioButtonIcon />}
+          </span>
+        </label>
+      </div>
+    );
+  }
 
   return (
     <div role={type} aria-checked={checked} tabIndex={0} onKeyDown={triggerOnEnter(onChange)}>
@@ -29,7 +59,7 @@ const RadioButton = (props: Props) => {
           checked={checked}
         />
 
-        <span className={styles.radioButtonCheckmark}>{checked ? <CheckCircleFillIcon /> : <CheckboxRadioIcon />}</span>
+        <span className={styles.radioButtonIcon}>{checked ? <CheckCircleFillIcon /> : <CheckboxRadioIcon />}</span>
       </label>
     </div>
   );
