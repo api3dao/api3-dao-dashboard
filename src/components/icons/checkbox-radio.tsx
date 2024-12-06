b50230762a3295d1f22c7ba14d26fad4b1ea646a
Peter Jurčo <62913177+peterjurco@users.noreply.github.com>
2024-12-06 16:33:50 +0100

    Fix console errors (#516)
    * Use camelCase in SVG element prop names when they are used as react
    components
    
    * Add forgotten key prop
    
    * Do not pass incorrect props to input & textarea

--- src/components/icons/checkbox-radio.tsx
+++ src/components/icons/checkbox-radio.tsx
@@ -12,7 +12,7 @@ export const CheckboxRadioIcon = ({ theme = 'light', ...props }: Props) => {
           d="M19.375 10C19.375 15.1777 15.1777 19.375 10 19.375C4.82233 19.375 0.625 15.1777 0.625 10C0.625 4.82233 4.82233 0.625 10 0.625C15.1777 0.625 19.375 4.82233 19.375 10Z"
           fill={theme === 'light' ? '#F8FAFD' : '#0C1143'}
           stroke={theme === 'light' ? '#99A0E4' : '#D1D5FD'}
-          stroke-width="1.25"
+          strokeWidth="1.25"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
