6a7bd0fcf5051d8dcb2884f6be7a7ca5b9b4eb4d
Peter Jurčo <62913177+peterjurco@users.noreply.github.com>
2024-12-09 08:32:58 +0100

    Rebrand Proposal details (#502)
    * Rebrand proposal details
    
    * Add testing capabilities
    
    * Fixes after the review
    
    * Use react components for error & success icons
    
    * Use useId()
    
    * Rename 'Target Address' to 'Target Contract Address'

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,9 +1,23 @@
-import { CheckboxRadioIcon } from './checkbox-radio';
+import { CheckCircleIcon } from './check-circle';
 import { CheckCircleFillIcon } from './check-circle-fill';
+import { CheckboxRadioIcon } from './checkbox-radio';
 import { CheckIcon } from './check';
 import { CloseIcon } from './close';
 import { CrossIcon } from './cross';
+import { ErrorCircleIcon } from './error-circle';
+import { ErrorCircleFillIcon } from './error-circle-fill';
 import { HelpOutlineIcon } from './help-outline';
 import { InfoCircleIcon } from './info-circle';
 
-export { CheckCircleFillIcon, CheckboxRadioIcon, CheckIcon, CloseIcon, CrossIcon, HelpOutlineIcon, InfoCircleIcon };
+export {
+  CheckCircleIcon,
+  CheckCircleFillIcon,
+  CheckIcon,
+  CheckboxRadioIcon,
+  CloseIcon,
+  CrossIcon,
+  ErrorCircleIcon,
+  ErrorCircleFillIcon,
+  HelpOutlineIcon,
+  InfoCircleIcon,
+};
