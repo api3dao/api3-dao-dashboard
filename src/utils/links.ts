c7c22ac99b861bcc173d14df871ad9c14521379a
Peter Jurčo <62913177+peterjurco@users.noreply.github.com>
2025-01-21 14:54:58 +0100

    Add privacy links & copyright to the footer (#527)
    * Add privacy links & copyright to the footer
    
    * Put links into utils file
    
    * Remove unused import

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,4 +1,8 @@
 export const links = {
-  SENTRY: 'https://sentry.io/',
+  API3_ORG: 'https://api3.org/',
+  GITHUB: 'https://github.com/api3dao/api3-dao-dashboard',
+  PRIVACY_AND_COOKIES: 'https://api3.org/privacy-and-cookies/',
   PRIVACY_POLICY: 'https://api3.org/privacy-policy/',
+  SENTRY: 'https://sentry.io/',
+  TERMS_AND_CONDITIONS: 'https://api3.org/terms-and-conditions/',
 };
