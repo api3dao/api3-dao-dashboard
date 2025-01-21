dd27a8b745af5913f7f9534564c1d25dc28211c0
Bogdan Iasinovschi <biasinovschi@gmail.com>
2025-01-21 21:12:39 +0200

    Rebrand toast messages (#519)
    * Update Button component
    
    * Preparatory changes
    
    * Update Toasts style
    
    * Update Clear Storage toast
    
    * Self review
    
    * Temporary testing toasts
    
    * Use customAction and prevent clear storage toast from closing
    
    * Fix Storage btn
    
    * Keep bg colour when autoClose is false
    
    * Remove testing code

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -7,10 +7,13 @@ import { CrossIcon } from './cross';
 import { ErrorCircleIcon } from './error-circle';
 import { ErrorCircleFillIcon } from './error-circle-fill';
 import { ExclamationTriangleFillIcon } from './exclamation-triangle-fill';
+import { ExternalLinkIcon } from './external-link';
 import { HelpOutlineIcon } from './help-outline';
 import { InfoCircleIcon } from './info-circle';
+import { InfoCircleFillIcon } from './info-circle-fill';
 import { RadioButtonIcon } from './radio-button';
 import { RadioButtonFillIcon } from './radio-button-fill';
+import { WarningCircleFillIcon } from './warning-circle-fill';
 
 export {
   CheckCircleIcon,
@@ -22,8 +25,11 @@ export {
   ErrorCircleIcon,
   ErrorCircleFillIcon,
   ExclamationTriangleFillIcon,
+  ExternalLinkIcon,
   HelpOutlineIcon,
   InfoCircleIcon,
+  InfoCircleFillIcon,
   RadioButtonIcon,
   RadioButtonFillIcon,
+  WarningCircleFillIcon,
 };
